8e62a9b250324f20de1475da03309378ef210233
MiaDRyu <miajoha.rhz@gmail.com>
2026-04-09 12:41:23 -0600

    Corrección

--- ProyectoNode/index.js
+++ ProyectoNode/index.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT || 23492, 
     ssl: {
         rejectUnauthorized: false
